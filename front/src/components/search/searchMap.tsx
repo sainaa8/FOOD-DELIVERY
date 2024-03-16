0dfbd80a4a6b9a863924b392f1efbf462338557e
ganhuyagdamdinnym <gdamdinnym@gmail.com>
2024-03-16 16:06:19 +0800

    yandaaaa

--- front/src/components/search/searchMap.tsx
+++ front/src/components/search/searchMap.tsx
@@ -4,7 +4,7 @@ import { Stack } from "@mui/material";
 import { Options } from "../home/OneModel";
 
 type Sda = {
-  data: FoodType;
+  data: FoodType[];
 };
 export const SearchMap = (props: Sda) => {
   const { data } = props;
@@ -22,11 +22,17 @@ export const SearchMap = (props: Sda) => {
           marginTop: "30px",
         }}
       >
-        {data?.map((el: FoodType, index: number) => (
-          <div key={index} id={el._id} x>
-            <Options zurag={el.image} text={el.name} une={el.price} />
+        {data.length > 0 ? (
+          <div>
+            {data?.map((el: FoodType, index: number) => (
+              <div key={index} id={el._id}>
+                <Options zurag={el.image} text={el.name} une={el.price} />
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div className="h-[100px] w-[100px]">hailt hooson</div>
+        )}
       </Stack>
     </Stack>
   );
